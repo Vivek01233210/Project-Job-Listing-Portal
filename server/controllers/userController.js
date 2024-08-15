971856905d118d5fcf918dd9de4ca432abda161e
Vivek Kumar <vivek3553.vr@gmail.com>
2024-08-15 20:57:16 +0530

    fix user-profile controller error

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -111,7 +111,7 @@ export const checkUser = async (req, res, next) => {
 };
 
 export const getUserProfile = async (req, res) => {
-    const user = await User.findById(req.user.id);
+    const user = await User.findById(req.user._id);
     if (!user) {
         return res.status(404).json({ error: 'User not found' });
     }
